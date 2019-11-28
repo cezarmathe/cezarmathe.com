c9ac15ee142cf917258701503477b50fd4ee14ad
cezarmathe <cezarmathe@gmail.com>
2019-11-28 08:40:58 +0200

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,30 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-rm -r public/*
+# Clone the build repository
+git clone "https://cezarmathe:${GITHUB_TOKEN}@github.com/cezarmathe/cezarmathe.github.io.git" public 1> /dev/null 2>&1
 
-# Build the project.
+# Build the website
 hugo -t hello-friend-ng --gc --minify
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+# Cd into the build folder
+cd public || exit 1
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+# Submit all changes
+git add -A
+
+# Commit changes
+commit_message="Rebuilding site: $(date -u +"%Y-%m-%dT%H:%M:%SZ")\n"
+if [ $# -eq 1 ]; then
+  commit_message="$1"
 fi
-git commit -m "$msg"
+git commit -a -m "${commit_message}"
 
-# Push source and build repos.
+# Push the new changes to the build repository
 git push origin master
 
-# Come Back up to the Project Root
+# Come back to the project root
 cd ..
+
+# Remove the build folder(and repository)
+rm --rf public
